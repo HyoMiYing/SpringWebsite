9ee3f3bf6d0aaccee0b4bf52612e8dbb9688d43c
rok <rklancar@gmail.com>
2023-03-03 17:20:12 +0100

    Added method to main class, which prints out all bean names, for troubleshooting and inspection purposes

--- src/main/java/rokklancar/com/rokklancar/RokklancarApplication.java
+++ src/main/java/rokklancar/com/rokklancar/RokklancarApplication.java
@@ -2,12 +2,22 @@ package rokklancar.com.rokklancar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RokklancarApplication {
+	private static ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
-		SpringApplication.run(RokklancarApplication.class, args);
+		applicationContext = SpringApplication.run(RokklancarApplication.class, args);
+		displayAllBeans();
+	}
+
+	public static void displayAllBeans() {
+		String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+		for(String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
 	}
 
 }
