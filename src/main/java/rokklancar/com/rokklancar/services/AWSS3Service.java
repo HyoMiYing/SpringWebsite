59257fed2f34f52db1db7dce6bf8933448a6f933
rok <rklancar@gmail.com>
2023-03-05 15:35:56 +0100

    Added AWS dependency and connected the project to AWS S3 service, to later get the mp3 file from it

---
+++ src/main/java/rokklancar/com/rokklancar/services/AWSS3Service.java
@@ -0,0 +1,25 @@
+package rokklancar.com.rokklancar.services;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AWSS3Service {
+
+
+    private static AWSCredentials credentials = new BasicAWSCredentials(
+            System.getenv("ACCESS_KEY"),
+            System.getenv("SECRET_ACCESS_KEY")
+    );
+
+    private static AmazonS3 s3client = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.EU_CENTRAL_1)
+            .build();
+}
