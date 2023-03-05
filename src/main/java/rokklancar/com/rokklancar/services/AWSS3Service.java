8fcb37d183e43450077ce69c9ade1418129ff078
rok <rklancar@gmail.com>
2023-03-05 15:38:00 +0100

    Added functionality where Spring reaches out to S3 bucket and retrieves the big mp3 file from it

--- src/main/java/rokklancar/com/rokklancar/services/AWSS3Service.java
+++ src/main/java/rokklancar/com/rokklancar/services/AWSS3Service.java
@@ -6,7 +6,13 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
 
 @Service
 public class AWSS3Service {
@@ -22,4 +28,18 @@ public class AWSS3Service {
             .withCredentials(new AWSStaticCredentialsProvider(credentials))
             .withRegion(Regions.EU_CENTRAL_1)
             .build();
+
+    public static void downloadAudiobookFile() {
+
+        File file = new File("src/main/resources/full_book.mp3");
+        if(!file.exists()) {
+            S3Object s3Object = s3client.getObject("ferdydurke-audio", "cela_knjiga.mp3");
+            S3ObjectInputStream inputStream = s3Object.getObjectContent();
+            try {
+                FileUtils.copyInputStreamToFile(inputStream, file);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
