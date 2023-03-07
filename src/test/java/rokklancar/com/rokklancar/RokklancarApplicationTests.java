23d503a43184afc3779e2649dfbaf2e5c8640dba
rok <rklancar@gmail.com>
2023-03-07 15:11:28 +0100

    Revert to last working state

--- src/test/java/rokklancar/com/rokklancar/RokklancarApplicationTests.java
+++ src/test/java/rokklancar/com/rokklancar/RokklancarApplicationTests.java
@@ -1,11 +1,9 @@
 package rokklancar.com.rokklancar;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class RokklancarApplicationTests {
 
 	@Test
