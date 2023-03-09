0caf028a7433443acdd7b3fcf0784265e7d41a4d
rok <rklancar@gmail.com>
2023-03-09 21:46:06 +0100

    Added InMemoryUSerDetailsManager with 3 users, for prototyping. used BCryptPasswordEncoder

--- src/main/java/rokklancar/com/rokklancar/configuration/SecurityConfiguration.java
+++ src/main/java/rokklancar/com/rokklancar/configuration/SecurityConfiguration.java
@@ -3,10 +3,37 @@ package rokklancar.com.rokklancar.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("rklancar@gmail.com")
+                .password(passwordEncoder().encode("yeayeeeea"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("liyough0@gmail.com")
+                .password(passwordEncoder().encode("ooohoooooo"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
